d32e3faca5606a6434f76fb7b5e73f999b022539
MythFN Owner <79332140+MythFNOwner@users.noreply.github.com>
2021-02-26 23:11:17 -0500

    Delete atlasfn.js

--- commands/atlasfn.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'atlasfn',
-    description: "this is the Dump  command",
-    execute(message, args){
-        message.channel.send('Why do you need AtlasFN if you have us? its skidded anyways');
-
-    }
-}
