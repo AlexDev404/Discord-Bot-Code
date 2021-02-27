8aebf53b7c4ce37a30c59326a29b9b70aa561322
MythFN Owner <79332140+MythFNOwner@users.noreply.github.com>
2021-02-27 00:26:30 -0500

    Delete kay.js

--- commands/kay.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'kay',
-    description: "Need kay to go to #memes use this command",
-    execute(message, args){
-        message.channel.send('OMG @kay IF YOU DO NOT STOP POSTING MEMES IN #GENERAL IMMA BEAT THAT ASS WITH MY BELT. USE #memes');
-
-    }
-}
